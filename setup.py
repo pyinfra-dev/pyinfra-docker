d4ed0c1aeefd395a5131944fc5cdd0ca5c3f2656
Nick Barrett <pointlessrambler@gmail.com>
2020-08-18 10:35:42 +0100

    Update setup args and add manifest.

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import find_packages, setup
 
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+
 if __name__ == '__main__':
     setup(
-        version='0.1',
+        version='1.0.dev0',
         name='pyinfra-docker',
+        description='Install & configure Docker with `pyinfra`.',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        author='Nick / Fizzadar',
+        author_email='pointlessrambler@gmail.com',
+        license='MIT',
         packages=find_packages(),
-        install_requires=('pyinfra>=0.5',),
+        install_requires=('pyinfra>=1',),
+        include_package_data=True,
     )
