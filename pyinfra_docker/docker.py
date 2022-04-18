eaf9eef297ebb761e837af77a2cc1a8e6b20151a
Nick Mills-Barrett <nick@fizzadar.com>
2022-04-18 15:36:59 +0100

    Upgrade to pyinfra v2

--- pyinfra_docker/docker.py
+++ pyinfra_docker/docker.py
@@ -1,30 +1,30 @@
 import json
+from io import StringIO
 
+from pyinfra import host
 from pyinfra.api.deploy import deploy
 from pyinfra.api.exceptions import DeployError
-from pyinfra.api.util import get_arg_value, make_hash
+from pyinfra.api.util import make_hash
+from pyinfra.facts.deb import DebPackages
+from pyinfra.facts.rpm import RpmPackages
+from pyinfra.facts.server import LsbRelease
 from pyinfra.operations import apt, files, yum
-from six.moves import StringIO
 
 
-def _apt_install(state, host):
+def _apt_install():
     apt.packages(
         name='Install apt requirements to use HTTPS',
         packages=['apt-transport-https', 'ca-certificates'],
         update=True,
         cache_time=3600,
-        state=state,
-        host=host,
     )
 
-    lsb_release = host.fact.lsb_release
+    lsb_release = host.get_fact(LsbRelease)
     lsb_id = lsb_release['id'].lower()
 
     apt.key(
         name='Download the Docker apt key',
         src='https://download.docker.com/linux/{0}/gpg'.format(lsb_id),
-        state=state,
-        host=host,
     )
 
     add_apt_repo = apt.repo(
@@ -33,25 +33,19 @@ def _apt_install(state, host):
             'deb [arch=amd64] https://download.docker.com/linux/{0} {1} stable'
         ).format(lsb_id, lsb_release['codename']),
         filename='docker-ce-stable',
-        state=state,
-        host=host,
     )
 
     apt.packages(
         name='Install Docker via apt',
         packages='docker-ce',
         update=add_apt_repo.changed,  # update if we added the repo
-        state=state,
-        host=host,
     )
 
 
-def _yum_install(state, host):
+def _yum_install():
     yum.repo(
         name='Add the Docker yum repo',
         src='https://download.docker.com/linux/centos/docker-ce.repo',
-        state=state,
-        host=host,
     )
 
     # Installing Docker on CentOS 8 is currently broken and requires this hack
@@ -65,13 +59,11 @@ def _yum_install(state, host):
         name='Install Docker via yum',
         packages=['docker-ce'],
         extra_install_args=extra_install_args,
-        state=state,
-        host=host,
     )
 
 
 @deploy('Deploy Docker')
-def deploy_docker(state=None, host=None, config=None):
+def deploy_docker(config=None):
     '''
     Install Docker on the target machine.
 
@@ -79,27 +71,24 @@ def deploy_docker(state=None, host=None, config=None):
         config: filename or dict of JSON data
     '''
 
-    if not host.fact.deb_packages and not host.fact.rpm_packages:
+    if not host.get_fact(DebPackages) and not host.get_fact(RpmPackages):
         raise DeployError((
             'Neither apt or yum were found, '
             'pyinfra-docker cannot provision this machine!'
         ))
 
     # Install Docker w/apt or yum
-    if host.fact.deb_packages:
-        _apt_install(state, host)
+    if host.get_fact(DebPackages):
+        _apt_install()
 
-    if host.fact.rpm_packages:
-        _yum_install(state, host)
+    if host.get_fact(RpmPackages):
+        _yum_install()
 
     config_file = config
 
     # If config is a dictionary, turn it into a JSON file for the config
     if isinstance(config, dict):
         config_hash = make_hash(config)
-
-        # Convert any jinja2 string variables ({{ host.data...}})
-        config = get_arg_value(state, host, config)
 
         # Turn into a file-like object and name such that we only generate one
         # operation hash between multiple hosts (with the same config).
@@ -110,14 +99,10 @@ def deploy_docker(state=None, host=None, config=None):
         files.directory(
             name='Ensure /etc/docker exists',
             path='/etc/docker',
-            state=state,
-            host=host,
         )
 
         files.put(
             name='Upload the Docker daemon.json',
             src=config_file,
             dest='/etc/docker/daemon.json',
-            state=state,
-            host=host,
         )
