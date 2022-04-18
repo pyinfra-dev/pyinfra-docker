aea1132e044bfb54b1066838cd6f74e00b915392
Nick Mills-Barrett <nick@fizzadar.com>
2022-04-18 18:18:55 +0100

    Cut v2.0

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 if __name__ == "__main__":
     setup(
-        version="1.0",
+        version="2.0",
         name="pyinfra-docker",
         description="Install & configure Docker with `pyinfra`.",
         long_description=readme,
         long_description_content_type="text/markdown",
         url="https://github.com/Fizzadar/pyinfra-docker",
         author="Nick / Fizzadar",
-        author_email="pointlessrambler@gmail.com",
+        author_email="nick@fizzadar.com",
         license="MIT",
         packages=find_packages(),
         install_requires=("pyinfra>=2,<3",),
